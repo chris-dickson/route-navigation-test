c26e66d929756d3445cefee80b573585facbe6ae
cdickson <cdickson@oculusinfo.com>
2016-01-30 08:34:36 -0500

    Remove welcome view from skeleton

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'documents', name: 'documents',      moduleId: 'documents',      nav: true, title: 'Documents' },
+      { route: ['','documents'], name: 'documents',      moduleId: 'documents',      nav: true, title: 'Documents' },
       { route: 'groups', name: 'groups',      moduleId: 'groups',      nav: true, title: 'Groups' }
 
     ]);
